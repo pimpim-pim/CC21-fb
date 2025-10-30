25d517d8b3b428445b95bd8a34150fd0167b8be6
pimpim-pim <pimzii.v.j@gmail.com>
2025-10-30 10:16:43 +0700

    identity to email or moblie zod

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,24 +1,18 @@
+import jwt from "jsonwebtoken"
 import bcrypt from "bcryptjs"
 import createHttpError from "http-errors"
 import identityKeyUtil from "../utils/identity-key.util.js"
 import prisma from "../config/prisma.config.js"
-import { registerSchema } from "../validations/schema.js"
+import { loginSchema, registerSchema } from "../validations/schema.js"
 
+//register
 export const register = async (req, res,next) => {
   const {identity, firstName, lastName, password, confirmPassword} = req.body
   //validation
-  // if(!identity.trim() || !firstName.trim() || !lastName.trim() || !password.trim() || !confirmPassword.trim()){
-  //   return next(createHttpError[400]('fill all inputs'))
-  // }
-  const rs = registerSchema.parse(req.body)
-  console.log(rs)
-
-  // if(confirmPassword !== password) {
-  //   return next(createHttpError[400]('check confirm-password'))
-  // }
-
-  //check Identity is emaill or mobile
-  const identityKey = identityKeyUtil(identity)
+  const user = registerSchema.parse(req.body)
+  // console.log(user)
+  //check Identity is email or mobile
+  const identityKey = user.email ? 'email' : 'mobile'
 
   if(!identityKey){
     return next(createHttpError[400]('identity must be email or phone number'))
@@ -32,12 +26,8 @@ export const register = async (req, res,next) => {
     return next(createHttpError[409]('This user already register'))
   }
 
-  const newUser = {
-    [identityKey] : identity,
-    password : await bcrypt.hash(password,10),
-    firstName : firstName,
-    lastName : lastName
-  }
+  const newUser = {...user, password : await bcrypt.hash(password,10),}
+  
   const result = await prisma.user.create({data : newUser})
     res.json({
       msg : 'Register Successful',
@@ -46,17 +36,48 @@ export const register = async (req, res,next) => {
 
 }
 
-export function login(req,res,next) {
-  // throw(new Error('This is my way'))
-  // if(req.body.password = "a1234"){
-  //   return next(createHttpError[400]('bad password'))
+//login
+export const login = async (req,res,next)=> {
+const {identity, password} = req.body
+//validate with zod
+const user = loginSchema.parse(req.body)
+// console.log(user)
+const identityKey = user.email ? 'email' : 'mobile'
+
+  // if(!identityKey){
+  //   return next(createHttpError[400]('identity must be email or phone number'))
   // }
+
+  const foundUser = await prisma.user.findUnique({
+    where : { [identityKey] : identity}
+  })
+
+  if(!foundUser) {
+    return next(createHttpError[400]('Invalid Login'))
+  }
+
+  //check password
+  let pwOk = await bcrypt.compare(password, foundUser.password)
+  if(!pwOk) {
+     return next(createHttpError[401]('Invalid Login'))
+  }
+
+  const payload = { id: foundUser.id }
+  const token = jwt.sign(payload, process.env.JWT_SECRET,{
+    algorithm: 'HS256',
+    expiresIn: '15d'
+  })
+  // console.log(token)
+  const {password: pw, createdAt, updatedAt,...userData} = foundUser
+ 
    res.json({
-   msg : 'Login Controller',
-   body : req.body
+   msg : 'Login Successful',
+   token : token,
+   user: userData
  })
 }
 
+//getMe
 export const getMe = (req,res) => {
  res.json({msg : 'GetMe controller'})
 }
